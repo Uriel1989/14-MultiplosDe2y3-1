14ee5ba367abfaa0d38dc431122b21222cc48f64
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:29:25 -0300

    Add files via upload

---
+++ 14-MultiplosDe2y3 1/14-MultiplosDe2y3 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _14_MultiplosDe2y3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Los multiplos de 2 son " + i);
+                }
+            }
+
+            for( i=0; i<=100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Los multiplos de 3 son " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
